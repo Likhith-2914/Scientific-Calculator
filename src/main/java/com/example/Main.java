91f59e5f3be3156055e1cce7f3a7761a66a98985
nvsl <nvsailikhith@gmail.com>
2023-11-06 14:38:34 +0530

    Added Log Files

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class Main 
 {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
